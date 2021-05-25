1de3a840c774e45fffc47301581d1d590ecf0788
Rômulo Fraga <romulo.fraga.dev@gmail.com>
2021-05-24 21:34:46 -0300

    Serviço de verificacao ADM

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import CreateSpecificationController from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import ListSpecificationController from "../../../../modules/cars/useCases/listSpecifications/ListSpecificationsController";
+import ensureAdmin from "../middlewares/ensureAdmin";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const specificationsRouter = Router();
@@ -9,9 +10,12 @@ const specificationsRouter = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationController();
 
-specificationsRouter.use(ensureAuthenticated);
-
-specificationsRouter.post("/", createSpecificationController.handle);
+specificationsRouter.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 specificationsRouter.get("/", listSpecificationController.handle);
 
